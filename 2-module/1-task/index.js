c31dca7c15b4ab066c6aa8714dffa39d123111e1
Darya <yankovets.daria@gmail.com>
2021-08-02 10:59:03 +0300

    add function sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for(let key in salaries) {
+    if (isFinite(salaries[key]) && salaries[key] !== NaN)  {
+      sum += salaries[key];
+    }
+  }
+
+  if (sum == 0) {
+    return 0;
+  }
+
+  return sum;
 }
